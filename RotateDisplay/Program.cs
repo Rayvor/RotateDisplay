4a1b00e8386b906604ab782494ada3a5b9c52593
Куксенков Андрей <neo.qlive@gmail.com>
2019-12-10 22:12:56 +0300

    Добавьте файлы проекта.

---
+++ RotateDisplay/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RotateDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        Display.Rotate(1, Display.Orientations.DEGREES_CW_0);
+                        break;
+                    case "1":
+                        Display.Rotate(1, Display.Orientations.DEGREES_CW_180);
+                        break;
+                    case "2":
+                        Display.Rotate(1, Display.Orientations.DEGREES_CW_90);
+                        break;
+                    case "3":
+                        Display.Rotate(1, Display.Orientations.DEGREES_CW_270);
+                        break;
+                }
+            }
+            
+        }
+    }
+}
